df1b7ef481f7751f93531b5f9ecbe7c8b415be84
ViktoriaArsenteva <vika.arsentievaa@yandex.ru>
2024-01-26 17:56:13 +0300

    add and connect swagger

--- src/main/java/ru/gb/springdemo/service/IssuerService.java
+++ src/main/java/ru/gb/springdemo/service/IssuerService.java
@@ -1,7 +1,6 @@
 package ru.gb.springdemo.service;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import ru.gb.springdemo.Entity.Issue;
